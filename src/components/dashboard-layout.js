eee05156d805de4d14af7618827482528943d5d9
mohamedboukari <mouhamedboukari20@gmail.com>
2023-02-12 21:37:41 +0100

    cleanup

--- src/components/dashboard-layout.js
+++ src/components/dashboard-layout.js
@@ -1,43 +1,34 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { AuthGuard } from "./auth-guard";
 import { DashboardNavbar } from "./dashboard-navbar";
-import { DashboardSidebar } from "./dashboard-sidebar";
 
-const DashboardLayoutRoot = styled("div")(({ theme }) => ({
+const DashboardLayoutRoot = styled("div")(() => ({
   display: "flex",
   flex: "1 1 auto",
   maxWidth: "100%",
   paddingTop: 64,
-  [theme.breakpoints.up("lg")]: {
-    paddingLeft: 280,
-  },
 }));
 
-export const DashboardLayout = ({ children }) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
-
-  return (
-    <AuthGuard>
-      <DashboardLayoutRoot>
-        <Box
-          sx={{
-            display: "flex",
-            flex: "1 1 auto",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          {children}
-        </Box>
-      </DashboardLayoutRoot>
-      <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
-      <DashboardSidebar onClose={() => setSidebarOpen(false)} open={isSidebarOpen} />
-    </AuthGuard>
-  );
-};
+export const DashboardLayout = ({ children }) => (
+  <AuthGuard>
+    <DashboardLayoutRoot>
+      <Box
+        sx={{
+          display: "flex",
+          flex: "1 1 auto",
+          flexDirection: "column",
+          width: "100%",
+        }}
+      >
+        {children}
+      </Box>
+    </DashboardLayoutRoot>
+    <DashboardNavbar />
+  </AuthGuard>
+);
 DashboardLayout.propTypes = {
   children: PropTypes.node,
 };
--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -1,12 +1,11 @@
 import { useRef, useState } from "react";
-import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Avatar, Box, IconButton, Toolbar, Tooltip } from "@mui/material";
+
 import SearchIcon from "@mui/icons-material/Search";
-import { Bell as BellIcon } from "../icons/bell";
+
 import { UserCircle as UserCircleIcon } from "../icons/user-circle";
-import { Users as UsersIcon } from "../icons/users";
+
 import { AccountPopover } from "./account-popover";
 
 const DashboardNavbarRoot = styled(AppBar)(({ theme }) => ({
@@ -15,23 +14,13 @@ const DashboardNavbarRoot = styled(AppBar)(({ theme }) => ({
 }));
 
 export const DashboardNavbar = (props) => {
-  const { onSidebarOpen, ...other } = props;
+  const { ...other } = props;
   const settingsRef = useRef(null);
   const [openAccountPopover, setOpenAccountPopover] = useState(false);
 
   return (
     <>
-      <DashboardNavbarRoot
-        sx={{
-          left: {
-            lg: 280,
-          },
-          width: {
-            lg: "calc(100% - 280px)",
-          },
-        }}
-        {...other}
-      >
+      <DashboardNavbarRoot {...other}>
         <Toolbar
           disableGutters
           sx={{
@@ -40,36 +29,13 @@ export const DashboardNavbar = (props) => {
             px: 2,
           }}
         >
-          <IconButton
-            onClick={onSidebarOpen}
-            sx={{
-              display: {
-                xs: "inline-flex",
-                lg: "none",
-              },
-            }}
-          >
-            <MenuIcon fontSize="small" />
-          </IconButton>
-
           <Tooltip title="Search">
             <IconButton sx={{ ml: 1 }}>
               <SearchIcon fontSize="small" />
             </IconButton>
           </Tooltip>
           <Box sx={{ flexGrow: 1 }} />
-          <Tooltip title="Contacts">
-            <IconButton sx={{ ml: 1 }}>
-              <UsersIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Notifications">
-            <IconButton sx={{ ml: 1 }}>
-              <Badge badgeContent={4} color="primary" variant="dot">
-                <BellIcon fontSize="small" />
-              </Badge>
-            </IconButton>
-          </Tooltip>
+
           <Avatar
             onClick={() => setOpenAccountPopover(true)}
             ref={settingsRef}
@@ -92,8 +58,4 @@ export const DashboardNavbar = (props) => {
       />
     </>
   );
-};
-
-DashboardNavbar.propTypes = {
-  onSidebarOpen: PropTypes.func,
 };
