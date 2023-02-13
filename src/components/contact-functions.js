4d545341568e526a74ed2467029d3509f78df142
Marouen Helali <marwanest@live.fr>
2023-02-13 03:18:19 +0100

    add twilio and emailjs

---
+++ src/components/contact-functions.js
@@ -0,0 +1,30 @@
+import emailjs from 'emailjs';
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+ 
+const handleSms = ({ values }) => {
+client.messages 
+      .create({          
+         to: values[0],
+         body: values[1]
+       }) 
+      .then(message => console.log(message.sid)) 
+      .done();
+
+    }
+const handleEmail = ({ values }) => {
+    emailjs
+      .send(
+        process.env.EMAIL_JS_SERVICE_ID,
+        process.env.EMAIL_JS_TEMPLATE_ID,
+        { name: values[0], email: values[1], subject: values[2], message: values[3] },
+        process.env.EMAIL_JS_PUBLIC_KEY
+      )
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        }
+      );
+  };
